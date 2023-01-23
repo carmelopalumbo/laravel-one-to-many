6a4ae9d61c4e4e524453c254a718d483db34a1bf
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-23 19:08:37 +0100

    fix-database-seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProjectsTableSeeder::class,
             TypesTableSeeder::class,
+            ProjectTypeTableSeeder::class
         ]);
     }
 }
